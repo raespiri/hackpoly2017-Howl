cdf063f299bf836bd63b3b4927685069ab004b9e
Sam <shuzawa@usc.edu>
2017-02-11 21:40:22 -0800

    images

--- Howl/Howl/NSTimerExtension.swift
+++ Howl/Howl/NSTimerExtension.swift
@@ -6,20 +6,20 @@
 //  Copyright © 2017 jonashen. All rights reserved.
 //
 
-//import Foundation
-//
-//extension Timer {
-//    static public func schedule(delay: TimeInterval, handler: @escaping (Timer!) -> Void) -> Timer {
-//        let fireDate = delay + CFAbsoluteTimeGetCurrent()
-//        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, handler)
-//        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
-//        return timer
-//    }
-//    
-//    static func schedule(repeatInterval interval: TimeInterval, handler: @escaping (Timer!) -> Void) -> Timer {
-//        let fireDate = interval + CFAbsoluteTimeGetCurrent()
-//        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, interval, 0, 0, handler)
-//        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
-//        return timer
-//    }
-//}
+import Foundation
+
+extension Timer {
+    static public func schedule(delay: TimeInterval, handler: @escaping (Timer!) -> Void) -> Timer {
+        let fireDate = delay + CFAbsoluteTimeGetCurrent()
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, handler)
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
+        return timer
+    }
+    
+    static func schedule(repeatInterval interval: TimeInterval, handler: @escaping (Timer!) -> Void) -> Timer {
+        let fireDate = interval + CFAbsoluteTimeGetCurrent()
+        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, interval, 0, 0, handler)
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
+        return timer
+    }
+}
