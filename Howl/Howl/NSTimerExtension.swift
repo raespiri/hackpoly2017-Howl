0188f78917e0cd5ccfdd70eab3fe600f6b60f9ee
Sam <shuzawa@usc.edu>
2017-02-11 21:37:00 -0800

    (frontend) mainpage

---
+++ Howl/Howl/NSTimerExtension.swift
@@ -0,0 +1,25 @@
+//
+//  NSTimerExtension.swift
+//  Howl
+//
+//  Created by Toshitaka on 2/11/17.
+//  Copyright © 2017 jonashen. All rights reserved.
+//
+
+//import Foundation
+//
+//extension Timer {
+//    static public func schedule(delay: TimeInterval, handler: @escaping (Timer!) -> Void) -> Timer {
+//        let fireDate = delay + CFAbsoluteTimeGetCurrent()
+//        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, 0, 0, 0, handler)
+//        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
+//        return timer
+//    }
+//    
+//    static func schedule(repeatInterval interval: TimeInterval, handler: @escaping (Timer!) -> Void) -> Timer {
+//        let fireDate = interval + CFAbsoluteTimeGetCurrent()
+//        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, fireDate, interval, 0, 0, handler)
+//        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopCommonModes)
+//        return timer
+//    }
+//}
